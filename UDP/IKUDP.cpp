3fc59f3a310ca44772441dfc4527751e49e2f9a2
inkcherry <344119449@qq.com>
2017-12-07 19:04:32 +0800

    package (recv and send)

--- UDP/IKUDP.cpp
+++ UDP/IKUDP.cpp
@@ -1,36 +1,83 @@
 ﻿#include "IKUDP.h"
-UDP::UDP()
-{
-	WSADATA wsa;
+#include "IKUDP.h"
+#include <system_error>
+#define DAYTIME_BUF_SIZE    64  /* 缓冲区的大小 */
+#define DAYTIME_DEF_COUNT    10  
+
+
+void IKUDP:: _init_wsadata() {
 	WSAStartup(WINSOCK_VERSION, &wsa);
 	iksock = socket(AF_INET, SOCK_DGRAM, 0);
 	if (iksock == INVALID_SOCKET)
-		std::cout << "未能初始化" << std::endl;
-	
+			throw std::system_error(WSAGetLastError(), std::system_category(), "Error opening socket");
+
 }
-
-
-
-UDP::~UDP()
+IKUDP::IKUDP(const std::string & address_, const int & port_):port(port_),flag(0)
 {
+	_init_wsadata();
+    _init_sockaddr(address_);  //初始化
+}
+IKUDP::IKUDP(addr_config &config_):port(config_.port), flag(0)
+{
+	_init_wsadata();
+	_init_sockaddr(config_.address);
 }
 
-void UDP::Send(const std::string& address_,unsigned short port,const char *buffer,int buffer_len)
+void IKUDP::_init_sockaddr(const std::string &address_)
+{
+	int flag = 1;												//暂定
+	addr.sin_family = AF_INET;
+	InetPton(AF_INET, address_.c_str(), &addr.sin_addr.s_addr);  //初始化sin_addr.s_addr
+	//	等价于 addr.sin_addr.s_addr = inet_addr(dest);
+	addr.sin_port = htons(port);
+}
+
+IKUDP::~IKUDP()
+{
+	closesocket(iksock);
+	WSACleanup();
+}
+bool IKUDP::Send(const std::string& buffer) 
+{
+	return Send(buffer.c_str(), buffer.length());
+}
+
+
+bool IKUDP::Send(const char *buffer,int buffer_len)
 {   
-	int flag = 1;
-	sockaddr_in add;
-	add.sin_family = AF_INET;
-	InetPton(AF_INET, address_.c_str() ,&add.sin_addr.s_addr);
-	add.sin_port = htons(port);
-
-	if (add.sin_addr.s_addr == INADDR_NONE)
+	
+	if (addr.sin_addr.s_addr == INADDR_NONE)
+		throw std::system_error(WSAGetLastError(), std::system_category(), "sendto failed");
+	int ret = sendto(iksock, buffer, buffer_len, 0,
+		(struct sockaddr *)&addr, sizeof(addr));
+	if (ret = buffer_len)      //完全发送算作发送成功
+		return true;
+	else  //发送失败
 	{
-		printf("[ECHO] invalid address\n");
-		
-	};
-
-	int ret = sendto(iksock, buffer, buffer_len, flag, reinterpret_cast<SOCKADDR *>(&add), sizeof(add));
-
-	if (ret < 0)
-		std::cout << "file to send";
+		std::cout << "send error" << ret << std::endl;
+		return false;
+	}
 }
+
+sockaddr_in IKUDP::Recv(char *buffer,int buffer_len)  
+{  
+	sockaddr_in peer_addr;  //from（默认的监听addr)
+	int peer_addr_size = sizeof(peer_addr);
+	int ret=recvfrom(iksock,buffer,buffer_len, flag,
+		(struct sockaddr *)&peer_addr, &peer_addr_size);
+	
+	//UDP无连接 故recvfrom返回值始终为0
+    //有连接时 TCP 0表示断开连接
+	return  peer_addr;
+}
+//sockaddr_in IKUDP::Recv(char *buffer, int buffer_len,bool is_block)
+//{
+//	sockaddr_in peer_addr;  //from（默认的监听addr)
+//	int peer_addr_size = sizeof(peer_addr);
+//	int ret = recvfrom(iksock, buffer, buffer_len, ,
+//		(struct sockaddr *)&peer_addr, &peer_addr_size);
+//
+//
+//	return  peer_addr;
+//}
+
--- UDP/IKUDP.h
+++ UDP/IKUDP.h
@@ -5,14 +5,34 @@
 #include <WS2tcpip.h>  //该头文件替代了WSA初始化
 #include <cwchar>
 #include <cstring>
-class UDP
+struct addr_config {
+	const std::string address;
+	const int port;
+};
+
+
+
+class IKUDP
 {
+//public: static void _init_wsadata_();
+//		int count_of_obj=0;
 public:
-	UDP();
-	~UDP();
-	void Send(const std::string& address_, unsigned short port, const char *buffer,int buffer_len);
+	IKUDP(const std::string &address_, const int &port_);
+	IKUDP(addr_config &config_);
+	~IKUDP();
+	bool Send(const char *buffer, int buffer_len);
+	bool Send(const std::string& buffer);  //发送出去但不一定保证接受到
+	sockaddr_in Recv(char *buffer, int buffer_len);
+	/*sockaddr_in Recv(char *buffer, int buffer_len, bool is_block);*/
+	/*bool Recv(std::string &buffer);*/ //重载一个string对象意义不大
 private:
-	SOCKET iksock;
-	
+	void _init_wsadata();   
+	void _init_sockaddr(const std::string &address);
+	std::string address;   
+	int port;			   
+	SOCKET iksock;  //socket对象
+	WSADATA wsa;  //用于iksock初始化
+	sockaddr_in addr;        
+	int flag;        //flag默认为0 考虑提供config配置
 };
 
