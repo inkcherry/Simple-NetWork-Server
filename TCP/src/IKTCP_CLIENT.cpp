0198a91ddc4f60c4e0be63982e08b48d1687e9cf
inkcherry <344119449@qq.com>
2018-01-06 19:04:17 +0800

    unblock

--- TCP/src/IKTCP_CLIENT.cpp
+++ TCP/src/IKTCP_CLIENT.cpp
@@ -1,12 +1,29 @@
 #include "/home/l/desktop/network/Simple-NetWork-Server/TCP/src/IKTCP_CLIENT.h"
 
 IKTCP_CLIENT::IKTCP_CLIENT(const std::string &address,const int &port):iksock(-1),port(0),address("")
-{
+{	_init_block_sock();
     _init_connect(address,port);
+}
+IKTCP_CLIENT::IKTCP_CLIENT(const std::string &address,const int &port,bool isunblock):iksock(-1),port(0),address("")
+{	if(isunblock==1) //设置为非阻塞
+	{_init_unblcok_sock();
+	_init_connect(address,port);}
+	else{//委托构造阻塞
+		IKTCP_CLIENT(address,port);
+	}
+}
+void IKTCP_CLIENT::_init_unblcok_sock()//定义阻塞或者非阻塞模式
+{
+    iksock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);//指定非阻塞
+
+}
+void IKTCP_CLIENT::_init_block_sock()
+{
+    iksock= socket(AF_INET , SOCK_STREAM , 0);
+
 }
 void IKTCP_CLIENT::_init_connect(const std::string &address,const int &port)
 {
-    iksock= socket(AF_INET , SOCK_STREAM , 0);
     if(iksock==-1)
         std::cout<<"errot with init sock";
     addr.sin_addr.s_addr = inet_addr( address.c_str() );
@@ -14,7 +31,7 @@ void IKTCP_CLIENT::_init_connect(const std::string &address,const int &port)
     addr.sin_port = htons( port );
 
     if (connect(iksock , (struct sockaddr *)&addr , sizeof(addr)) < 0)
-        std::cout<<"errot with init connect";
+		std::cout<<"unblock connect";
 }
 
 bool IKTCP_CLIENT::Send(const std::string &buffer)
@@ -27,12 +44,13 @@ bool IKTCP_CLIENT::Send(const std::string &buffer)
 	return true;
 }
 std::string IKTCP_CLIENT::Recv(const int buffer_len)
-{   
+{
   	char buffer[buffer_len];
-  
-	if( recv(iksock , buffer,buffer_len, 0) < 0)// sizeof(buffer) 
+
+	if( recv(iksock , buffer,buffer_len, 0) < 0)// sizeof(buffer)
   	{
-	    std::cout << "receive failed!" << std::endl;
+		// std::cout << "receive failed!" << std::endl;
+			return "";
   	}
 	buffer[buffer_len]='\0';
   	return (std::string)buffer;
--- TCP/src/IKTCP_CLIENT.h
+++ TCP/src/IKTCP_CLIENT.h
@@ -14,10 +14,15 @@ class IKTCP_CLIENT
 {
 public:
     IKTCP_CLIENT(const std::string &address ,const  int &port);
+    IKTCP_CLIENT(const std::string &address ,const  int &port,bool isunblock);
     bool Send(const std::string &buffer);
     std::string Recv(const int buffer_len=128);
+    void set_block_config(bool isunblock);  //设置阻塞属性 0为阻塞 1为非阻塞
 private:
     std::string address;
+    void _init_unblcok_sock();//定义阻塞或者非阻塞模式
+    void _init_block_sock();//定义阻塞或者非阻塞模式
+
     void _init_connect(const std::string &address,const int &port);
     int iksock;
 
